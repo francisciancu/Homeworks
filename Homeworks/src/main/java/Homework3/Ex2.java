19dcb2b588b8ad607a1fef2e5d904f94e0ecc801
Francisc Andrei Sebastian Iancu <andreiiancu14@gmail.com>
2023-05-26 09:11:04 +0300

    Update folders

---
+++ Homeworks/src/main/java/Homework3/Ex2.java
@@ -0,0 +1,21 @@
+package Homework3;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your score?");
+        int score = scanner.nextInt();
+
+        if (score>=0 && score<=65){
+            System.out.println("Ai picat. Mai incearca");
+
+        } else if (score>=66){
+            System.out.println("Felicitari, Ai trecut");
+
+        }
+    }
+}
