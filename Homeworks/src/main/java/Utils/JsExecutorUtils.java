d13c016c7651c706e3e11317ba47f7a3316372b4
Francisc Andrei Sebastian Iancu <andreiiancu14@gmail.com>
2023-09-26 14:06:20 +0300

    Homework31+refactor

--- Homeworks/src/main/java/Utils/JsExecutorUtils.java
+++ Homeworks/src/main/java/Utils/JsExecutorUtils.java
@@ -4,51 +4,49 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import static Utils.RandomTextGenerator.generateRandomString;
-
 public class JsExecutorUtils {
-    private static JavascriptExecutor js;
-    private static final String javascriptHover = "var obiect = document.createEvent('MouseEvent');"
+    private static JavascriptExecutor JS;
+    private static final String JAVASCRIPT_HOVER = "var obiect = document.createEvent('MouseEvent');"
             + "obiect.initMouseEvent('mouseover', true);"
             + "arguments[0].dispatchEvent(obiect);";
-    private static final String javascriptScrollIntoView = "arguments[0].scrollIntoView();";
+    private static final String JAVASCRIPT_SCROLL_INTO_VIEW = "arguments[0].scrollIntoView();";
 
-    private static final String javascriptCheckVisibility = "return arguments[0].checkVisibility()";
-    private static final String javascriptClick = "arguments[0].click();";
-    private static final String javascriptOffsetHeight = "return arguments[0].offsetHeight;";
-    private static final String javascriptGetText = "return arguments[0].innerText;";
-    private static final String javascriptSendKeys = "arguments[0].value='";
+    private static final String JAVASCRIPT_CHECK_VISIBILITY = "return arguments[0].checkVisibility()";
+    private static final String JAVASCRIPT_CLICK = "arguments[0].click();";
+    private static final String JAVASCRIPT_OFFSET_HEIGHT = "return arguments[0].offsetHeight;";
+    private static final String JAVASCRIPT_GET_TEXT = "return arguments[0].innerText;";
+    private static final String JAVASCRIPT_SEND_KEYS = "arguments[0].value='";
 
     public static void initialize(WebDriver driver) {
-        js = (JavascriptExecutor) driver;
+        JS = (JavascriptExecutor) driver;
     }
 
     public static void hoverOverElement(WebElement element) {
-        js.executeScript(javascriptHover, element);
+        JS.executeScript(JAVASCRIPT_HOVER, element);
     }
 
     public static void clickOnElement(WebElement element) {
-        js.executeScript(javascriptClick, element);
+        JS.executeScript(JAVASCRIPT_CLICK, element);
     }
 
     public static Boolean checkVisibilityOfElement(WebElement element) {
-        return (Boolean) js.executeScript(javascriptCheckVisibility, element);
+        return (Boolean) JS.executeScript(JAVASCRIPT_CHECK_VISIBILITY, element);
     }
 
     public static void scrollElementIntoView(WebElement element) {
-        js.executeScript(javascriptScrollIntoView, element);
+        JS.executeScript(JAVASCRIPT_SCROLL_INTO_VIEW, element);
     }
 
     public static void moveUpToAvoidStickyElementCovering(WebElement element) {
-        var stickyOffset = js.executeScript(javascriptOffsetHeight, element).toString();
-        js.executeScript("window.scrollBy(0,-" + stickyOffset + ")", element);
+        var stickyOffset = JS.executeScript(JAVASCRIPT_OFFSET_HEIGHT, element).toString();
+        JS.executeScript("window.scrollBy(0,-" + stickyOffset + ")", element);
     }
 
     public static String getTextOfElement(WebElement element) {
-        return js.executeScript(javascriptGetText, element).toString();
+        return JS.executeScript(JAVASCRIPT_GET_TEXT, element).toString();
     }
 
     public static void sendKeysToField(String text,WebElement element) {
-        js.executeScript(javascriptSendKeys +text+"'",element);
+        JS.executeScript(JAVASCRIPT_SEND_KEYS +text+"'",element);
     }
 }
