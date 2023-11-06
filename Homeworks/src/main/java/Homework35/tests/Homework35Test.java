06fdaaa0a262add89ab1abbefc307b8a3d254781
Francisc Andrei Sebastian Iancu <andreiiancu14@gmail.com>
2023-11-06 13:59:08 +0200

    Added custom logging messages for methods in MyMethods

--- Homeworks/src/main/java/Homework35/tests/Homework35Test.java
+++ Homeworks/src/main/java/Homework35/tests/Homework35Test.java
@@ -3,14 +3,15 @@ package Homework35.tests;
 import Homework35.pages.HomePage;
 import KeyDriver.BaseTestKeyFood;
 import Utils.DataProviderClass;
-import static Utils.EnumCategories.*;
-
 import org.testng.annotations.Test;
+
+import static Utils.EnumCategories.*;
 
 public class Homework35Test extends BaseTestKeyFood {
     @Test(dataProviderClass = DataProviderClass.class, dataProvider = "keyFoodAccount")
     public void homework35Test(String user, String pass) {
-
+        //TODO: asserturi cu generic type <T>
+        //TODO: log4J -> Jcabi Log culori
         new HomePage()
                 .clickOnAccountToLogin()
                 .inputUsername(user)
--- Homeworks/src/main/java/Utils/MyMethods.java
+++ Homeworks/src/main/java/Utils/MyMethods.java
@@ -1,6 +1,9 @@
 package Utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,118 +12,304 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MyMethods {
     private static WebDriver driver;
-    private static WebDriverWait wait;
+    private static final Logger logger = LogManager.getLogger(MyMethods.class);
 
     public MyMethods() {
     }
 
     public static void setWebDriver(WebDriver webDriver) {
         driver = webDriver;
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public static boolean isVisible(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(element)).isDisplayed();
     }
 
     public static boolean isVisible(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.visibilityOf(driver.findElement(by))).isDisplayed();
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(driver.findElement(by))).isDisplayed();
     }
 
     public static boolean isClickable(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.elementToBeClickable(element)).isEnabled() && wait.until(ExpectedConditions.elementToBeClickable(element)).isDisplayed();
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(element)).isEnabled() && new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(element)).isDisplayed();
     }
 
     public static boolean isClickable(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        return wait.until(ExpectedConditions.elementToBeClickable(by)).isEnabled() && wait.until(ExpectedConditions.elementToBeClickable(by)).isDisplayed();
+        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(by)).isEnabled() && new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(by)).isDisplayed();
     }
+
     public static void waitForReadyState() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+
+        try {
+            logger.info("Waiting for ready state");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.jsReturnsValue("return document.readyState === 'complete';"));
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
+
     public static void waitForElementToBeClickable(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(by));
+        try {
+            logger.info("Waiting for element with selector [ " + by + " ] to be clickable");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(by));
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
+
     }
+
     public static void waitForElementToBeVisible(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(by)));
+        try {
+            logger.info("Waiting for element with selector [ " + by + " ] to be visible");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(driver.findElement(by)));
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
+
     }
+
     public static void waitForElementToUpdate(By by, String initialText) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(by, initialText)));
+        try {
+            logger.info("Waiting for element with selector [ " + by + " ] text to not be [ " + initialText + " ]");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.not(ExpectedConditions.textToBe(by, initialText)));
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
+
     public static void waitForElementStaleness(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.stalenessOf(driver.findElement(by)));
+        try {
+            logger.info("Waiting for staleness of element with selector [ " + by + " ]");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.stalenessOf(driver.findElement(by)));
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
+
     }
+
     public static void waitForUrlToNotBe(String url) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
+        try {
+            logger.info("Waiting for URL to not be [ " + url + " ]");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.not(ExpectedConditions.urlToBe(url)));
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
+
     public static void waitForUrlToBe(String url) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.urlToBe(url));
+        try {
+            logger.info("Waiting for URL to be [ " + url + " ]");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.urlToBe(url));
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
+
     public static void waitForElementNotStaleness(By by) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.not(ExpectedConditions.stalenessOf(driver.findElement(by))));
+        try {
+            logger.info("Waiting for not staleness of element with selector [ " + by + " ]");
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.not(ExpectedConditions.stalenessOf(driver.findElement(by))));
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
 
     public static void clickOnElement(WebElement element) {
-        element.click();
+        try {
+            element.click();
+            logger.info("Successfully clicked on element [ " + element + " ]");
+        } catch (NoSuchElementException e) {
+            logger.error("Element [ " + element + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
 
     public static void clickOnElement(By by) {
-        driver.findElement(by).click();
+
+        try {
+            driver.findElement(by).click();
+            logger.info("Successfully clicked on element with selector [ " + by + " ]");
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
 
     public static void sendKeys(By by, String keys) {
-        driver.findElement(by).sendKeys(keys);
+        logger.info("Trying to send keys: " + keys + " to element [ " + by + " ]");
+        try {
+            var inputField = driver.findElement(by);
+            inputField.sendKeys(keys);
+            if (inputField.getAttribute("type").equals("password")) {
+                logger.info("Successfully sent password " + "*".repeat(new Random().nextInt((5) + 5)) + " to element [ " + by + " ]");
+            } else {
+                logger.info("Successfully sent keys: " + keys + " to element [ " + by + " ]");
+            }
+
+        } catch (NoSuchElementException e) {
+            logger.error("Element [ " + by + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
 
     public static void sendKeys(WebElement element, String keys) {
-        element.sendKeys(keys);
+        logger.info("Trying to send keys: " + keys + " to element [ " + element + " ]");
+        try {
+            element.sendKeys(keys);
+            logger.info("Successfully sent keys: " + keys + " to element [ " + element + " ]");
+        } catch (NoSuchElementException e) {
+            logger.error("Element [ " + element + " ] not found: " + e.getMessage());
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
+
     }
 
     public static String getCurrentUrl() {
-        return driver.getCurrentUrl();
+        logger.info("Trying to get current URL");
+        try {
+            var currentUrl = driver.getCurrentUrl();
+            logger.info("Successfully got current URL");
+            return currentUrl;
+
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+            return null;
+        }
     }
 
+
     public static String getTextFromElement(By by) {
-        return driver.findElement(by).getText();
+        logger.info("Trying to get text form element with selector [ " + by + " ]");
+        try {
+            var elementText = driver.findElement(by).getText();
+            logger.info("Successfully got text form element with selector [ " + by + " ]");
+            return elementText;
+
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+            return null;
+        }
     }
 
     public static String getTextFromElement(WebElement element) {
-        return element.getText();
-    }
-    public static List<String> getListOfTextFromElements(List<WebElement> elements) {
-        var textList = new ArrayList<String>();
-        for (var element: elements) {
-            textList.add(getTextFromElement(element));
+        var elementSelector = extractElementSelector(element);
+        logger.info("Trying to get text form element with selector [ " + elementSelector + " ]");
+        try {
+            var elementText = element.getText();
+            logger.info("Successfully got text form element with selector[ " + elementSelector + " ]");
+            return elementText;
+
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + elementSelector + " ] not found: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+            return null;
         }
-        return textList;
+    }
+
+    public static List<String> getListOfTextFromElements(List<WebElement> elements) {
+        var elementSelectors = extractElementSelectors(elements);
+        logger.info("Trying to get text form list of elements with selector [ " + elementSelectors + " ]");
+        try {
+            var textList = new ArrayList<String>();
+            for (var element : elements) {
+                textList.add(getTextFromElement(element));
+            }
+            logger.info("Successfully got text form list of elements with selector[ " + elementSelectors + " ]");
+            return textList;
+
+        } catch (NoSuchElementException e) {
+            logger.error("Elements with selector [ " + elementSelectors + " ] not found: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+            return null;
+        }
     }
 
     public static List<WebElement> elements(By by) {
-        return driver.findElements(by);
+        logger.info("Trying to get elements with selector [ " + by + " ]");
+        try {
+            var elements = driver.findElements(by);
+            logger.info("Successfully got elements with selector [ " + by + " ]");
+            return elements;
+
+        } catch (NoSuchElementException e) {
+            logger.error("Elements with selector [ " + by + " ] not found: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+            return null;
+        }
     }
 
     public static WebElement element(By by) {
-        return driver.findElement(by);
+        logger.info("Trying to get element with selector [ " + by + " ]");
+        try {
+            var element = driver.findElement(by);
+            logger.info("Successfully got element with selector [ " + by + " ]");
+            return element;
+
+        } catch (NoSuchElementException e) {
+            logger.error("Element with selector [ " + by + " ] not found: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+            return null;
+        }
     }
 
     public static void goBack() {
-        driver.navigate().back();
+        logger.info("Trying to got back 1 time");
+        try {
+            driver.navigate().back();
+            logger.info("Successfully went back 1 time");
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
     }
+
     public static void refreshPage() {
-        driver.navigate().refresh();
+        logger.info("Trying to refresh page 1 time");
+        try {
+            driver.navigate().refresh();
+            logger.info("Successfully refreshed page 1 time");
+        } catch (Exception e) {
+            logger.error("An error occurred: " + e.getMessage());
+        }
+    }
+
+    public static String extractElementSelector(WebElement element) {
+        String elementSelector = element.toString();
+        return elementSelector.replaceAll(".*? -> (.+?)]", "$1");
+    }
+
+    public static List<String> extractElementSelectors(List<WebElement> elements) {
+        List<String> selectors = new ArrayList<>();
+        for (WebElement element : elements) {
+            selectors.add(extractElementSelector(element));
+        }
+        return selectors;
     }
 }
